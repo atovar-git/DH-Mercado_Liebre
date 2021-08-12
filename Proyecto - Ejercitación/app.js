0c9ebfd83e9d9110372d69be21a072490208f7c3
Alan Victor Tovar <alanvictortovar@hotmail.com>
2021-08-11 21:45:08 -0300

    feat:First commit

---
+++ Proyecto - Ejercitación/app.js
@@ -0,0 +1,13 @@
+const express = require('express');     // Levantar/crear un servidor.
+const app = express();                  // Debemos guardar la ejecución de la función en un variable llamada app generalmente.
+
+const path = require('path');           // Me permite independizar del sistema operativo y que no haya inconvenientes con las rutas.
+
+app.use(express.static(path.resolve(__dirname, './public')));   // Indicamos a Express que esta carpeta será pública.
+
+app.listen(3000, () => console.log('Servidor corriendo en el puerto 3000'));   // Levantamos el servidor que estará escuchando peticiones en el puerto 3000.
+
+// Ruta para la raíz:
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'))
+})
